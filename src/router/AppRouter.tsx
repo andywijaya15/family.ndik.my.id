d82e3fc11b0518eec457ebfaa113b745997c5296
andywijaya15 <andykevonata@gmail.com>
2025-11-28 14:05:23 +0700

    chunck

--- src/router/AppRouter.tsx
+++ src/router/AppRouter.tsx
@@ -1,52 +1,59 @@
 import { AuthProvider } from "@/context/AuthContext";
 import Login from "@/pages/Auth/Login";
 import Home from "@/pages/Home";
-import { MasterCategory } from "@/pages/Master/MasterCategory";
-import { MasterTransaction } from "@/pages/Master/MasterTransaction";
+import { lazy, Suspense } from "react";
 import { BrowserRouter, Navigate, Route, Routes } from "react-router";
 import RouteGuard from "./RouteGuard";
+const MasterCategory = lazy(() =>
+  import("@/pages/Master/MasterCategory").then((mod) => ({ default: mod.MasterCategory }))
+);
+const MasterTransaction = lazy(() =>
+  import("@/pages/Master/MasterTransaction").then((mod) => ({ default: mod.MasterTransaction }))
+);
 
 export const AppRouter = () => {
   return (
     <>
       <AuthProvider>
         <BrowserRouter>
-          <Routes>
-            <Route path="/" element={<Navigate to="/home" replace />} />
+          <Suspense fallback={<div>Loading...</div>}>
+            <Routes>
+              <Route path="/" element={<Navigate to="/home" replace />} />
 
-            <Route
-              path="/home"
-              element={
-                <RouteGuard>
-                  <Home />
-                </RouteGuard>
-              }
-            />
-            <Route
-              path="/category"
-              element={
-                <RouteGuard>
-                  <MasterCategory />
-                </RouteGuard>
-              }
-            />
-            <Route
-              path="/transaction"
-              element={
-                <RouteGuard>
-                  <MasterTransaction />
-                </RouteGuard>
-              }
-            />
-            <Route
-              path="/login"
-              element={
-                <RouteGuard publicOnly>
-                  <Login />
-                </RouteGuard>
-              }
-            />
-          </Routes>
+              <Route
+                path="/home"
+                element={
+                  <RouteGuard>
+                    <Home />
+                  </RouteGuard>
+                }
+              />
+              <Route
+                path="/category"
+                element={
+                  <RouteGuard>
+                    <MasterCategory />
+                  </RouteGuard>
+                }
+              />
+              <Route
+                path="/transaction"
+                element={
+                  <RouteGuard>
+                    <MasterTransaction />
+                  </RouteGuard>
+                }
+              />
+              <Route
+                path="/login"
+                element={
+                  <RouteGuard publicOnly>
+                    <Login />
+                  </RouteGuard>
+                }
+              />
+            </Routes>
+          </Suspense>
         </BrowserRouter>
       </AuthProvider>
     </>
